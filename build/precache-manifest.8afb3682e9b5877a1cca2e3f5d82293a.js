ba386e7aacddc0b9e7c31184d45106dfad80e049
lumingyuan <lumingyuan@sunkfa.com>
2020-01-17 10:27:27 +0800

    lu

---
+++ build/precache-manifest.8afb3682e9b5877a1cca2e3f5d82293a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d396835e761746c2b3bb60a71b4807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7b44dc9ca8318d10ff",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "944b9cf7615e1069e314",
+    "url": "/static/js/2.53a8c91c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.53a8c91c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a7b44dc9ca8318d10ff",
+    "url": "/static/js/main.a36059e7.chunk.js"
+  },
+  {
+    "revision": "29c4995013be4954e500",
+    "url": "/static/js/runtime-main.dcc7b5e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
